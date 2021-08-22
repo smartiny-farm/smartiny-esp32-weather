6e3133f4fee492368fbcb2dcfb2090987f788960
Joao Pedro Girardi <girardijp@hotmail.com>
2021-08-22 03:01:59 -0300

    refactor: cleaned unused code

--- src/main.cpp
+++ src/main.cpp
@@ -10,22 +10,6 @@ void setup() {
 
 void loop() {
   SensorsHandler sensors(4);
-  /*InfluxDBClient client(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKEN, InfluxDbCloud2CACert);
-  Point sensor("wifi_status_tw");
-  if (client.validateConnection()) {
-    Serial.print("Connected to InfluxDB: ");
-    Serial.println(client.getServerUrl());
-  } else {
-    Serial.print("InfluxDB connection failed: ");
-    Serial.println(client.getLastErrorMessage());
-  }
-  //Point pointDevice("device_status");
-  sensor.addTag("device", DEVICE);
-  sensor.addTag("SSID", WiFi.SSID());
-  timeSync(TZ_INFO, "pool.ntp.org", "time.nis.gov");
-  sensor.clearFields();
-  sensor.addField("rssi", WiFi.RSSI());
-  client.writePoint(sensor);*/
   while(1){
     sensors.ConfigureVector();
     sensors.getSensorsData();
